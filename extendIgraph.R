4752b0da253fb4b948619488840e8f31fd1e0561
ggirelli <gabriele@filopoe.it>
2014-05-13 12:20:18 +0200

    Implemented vertex comparison and contains_vertex

--- extendIgraph.R
+++ extendIgraph.R
@@ -1,48 +1,60 @@
 "==.igraph.vs" = function(x, y) {
-	attr.list.x <- list.vertex.attributes(get('graph', attr(x, 'env')))
-	attr.list.y <- list.vertex.attributes(get('graph', attr(y, 'env')))
-
-	if(length(attr.list.x) != length(attr.list.y)) return(F);
-	if(length(which(attr.list.x %in% attr.list.y)) != length(attr.list.x)) return(F);
-
-	attr.value.x <- lapply(attr.list.x, x, FUN=function(attr, x) { return( eval(parse(text=paste0('x$', attr))) ); })
-	attr.value.y <- lapply(attr.list.y, y, FUN=function(attr, y) { return( eval(parse(text=paste0('y$', attr))) ); })
+	# Can compare a couple of vertices, or a vertex to a vertex vector. The comparison is carried out on the attributes of each vertex (except for the id).
 	
-	if(length(which(as.logical(lapply(attr.value.x, attr.value.y, FUN=function(x,y) { return(x %in% y); })))) != length(attr.list.x)) return(F);
+	# If both node lists have length == 1 perform pair comparison
+	if(length(x) == 1 && length(y) == 1) {
 
-	return(T)
+		# Retrieve attribute name list
+		attr.list.x <- list.vertex.attributes(get('graph', attr(x, 'env')))
+		attr.list.y <- list.vertex.attributes(get('graph', attr(y, 'env')))
+
+		# Check attribute name list
+		if(length(attr.list.x) != length(attr.list.y)) return(FALSE);
+		if(length(which(attr.list.x %in% attr.list.y)) != length(attr.list.x)) return(FALSE);
+
+		# Retrieve attribute values
+		attr.value.x <- lapply(attr.list.x, x, FUN=function(attr, x) { return( eval(parse(text=paste0('x$', attr))) ); })
+		attr.value.y <- lapply(attr.list.y, y, FUN=function(attr, y) { return( eval(parse(text=paste0('y$', attr))) ); })
+
+		# Verify identity
+		if(length(which(vapply(attr.value.x, FUN=function(x,y) { return(x %in% unlist(y)) }, FUN.VALUE=c(logical(1), logical(0)), y=attr.value.y))) == 0) return(FALSE)
+		return(TRUE)
+
+	} else { # Other cases:
+
+		if(length(x) == 1) { # x is singular while y is not
+			return(vapply(y, FUN=function(x, y, env) { if(y == V(get('graph', env))[x]) return(TRUE); return(FALSE) }, FUN.VALUE=c(logical(1),logical(0)), y=x, env=attr(y, 'env')))
+		} else if(length(y) == 1) { # y is singular while x is not
+			cat('Error: cannot compare node vector to single node.\n')
+			return(NULL)
+		} else {
+			# Both x and y are note singular
+			# They have different length
+			if(length(x) != length(y)) {
+				cat('Error: can compare only node vectors with the same length.')
+				return(NULL)
+			}
+			# They have the same length
+			cat('Compared node vectors ina pair-wise fashion.\n')
+			return(vapply(x, FUN=function(x, y, env) { if(V(get('graph', env))[x] == y[x]) return(TRUE); return(FALSE) }, FUN.VALUE=c(logical(1),logical(0)), y=y, env=attr(x, 'env')))
+		}
+	}
 }
 
-has.vertex = function(x, y) {
+"%in%" = function(x, y) {
+	# Extended to allow operations with igraph.vs (nodes)
 	
-}
-
-"unique.igraph.vs" = function(x) {
-	h <- graph.empty()
-
-	attr.list <- list.vertex.attributes(get('graph', attr(x, 'env')))
-
-	return(V(h))
-}
-
-
-
-
-
-
-
-
-"==.[.igraph.es" = function(x, y) {
-	attr.list.x <- list.edge.attributes(get('graph', attr(x, 'env')))
-	attr.list.y <- list.edge.attributes(get('graph', attr(y, 'env')))
-
-	if(length(attr.list.x) != length(attr.list.y)) return(F);
-	if(length(which(attr.list.x %in% attr.list.y)) != length(attr.list.x)) return(F);
-
-	attr.value.x <- lapply(attr.list.x, x, FUN=function(attr, x) { return( eval(parse(text=paste0('x$', attr))) ); })
-	attr.value.y <- lapply(attr.list.y, y, FUN=function(attr, y) { return( eval(parse(text=paste0('y$', attr))) ); })
-	
-	if(length(which(as.logical(lapply(attr.value.x, attr.value.y, FUN=function(x,y) { return(x %in% y); })))) != length(attr.list.x)) return(F);
-
-	return(T)
+	if(class(x) == class(y) && class(x) == 'igraph.vs') {
+		if(length(y) == 0 || length(x) == 0) {
+			cat('Error: cannot compare NULL\n')
+			return(NULL)
+		}
+		if(length(x) == 1 && length(y) == 1) return(x == y)
+		if(length(x) == 1 && length(y) > 1) return(length(which(x == y)) != 0)
+		if(length(y) == 1 && length(x) > 1) return(length(which(y == x)) != 0)
+		cat("Error: wrong lengths\n")
+		return(NULL)
+	} else { # Old %in%
+		match(x, y, nomatch = 0L) > 0L
+	}
 }
