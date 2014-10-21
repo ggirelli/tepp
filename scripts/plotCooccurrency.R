6ec2f6a4ebd53bc3a858026f850a11baac951311
ggirelli <gabriele@filopoe.it>
2014-10-21 12:09:46 +0200

    Fixed co-occurrency and occurrency plot scripts.

--- scripts/plotCooccurrency.R
+++ scripts/plotCooccurrency.R
@@ -1,14 +1,14 @@
 #!/usr/bin/env Rscript
 #
-# ./plotCooccurrency.R total_graph suffix output_dir
+# ./plotCooccurrency.R total_graph n.sample suffix output_dir
 
 library('igraph')
 
 args <- commandArgs(trailingOnly=TRUE)
-if(length(args) != 3) stop('./plotCooccurrency.R total_graph suffix output_dir')
+if(length(args) != 4) stop('./plotCooccurrency.R total_graph n.sample suffix output_dir')
 
 # Mkdir
-if(!file.exists(args[3])) dir.create(args[3])
+if(!file.exists(args[4])) dir.create(args[4])
 
 # Read
 g <- read.graph(args[1], format='graphml')
@@ -18,35 +18,35 @@ cc <- as.numeric(E(g)$clonal.cooc)
 ss <- as.numeric(E(g)$subclonal.cooc)
 nn <- as.numeric(E(g)$nonclonal.cooc)
 
-x.max <- max(c(max(cs), max(cc), max(ss), max(nn)))
-y.max <- max(c(max(density(cs)$y), max(density(cc)$y), max(density(ss)$y), max(density(nn)$y)))
-breaks.cs <- seq(-0.5, max(cs)+1)
-breaks.cc <- seq(-0.5, max(cc)+1)
-breaks.ss <- seq(-0.5, max(ss)+1)
-breaks.nn <- seq(-0.5, max(nn)+1)
-h.cs <- hist(cs, main='Edge distribution', xlab='number of samples', xlim=c(0, x.max), breaks=breaks.cs, prob=T, plot=F)
-h.cc <- hist(cc, main='Clonal cooccurrency distribution', xlab='number of samples', xlim=c(0, x.max), breaks=breaks.cc, prob=T, plot=F)
-h.ss <- hist(ss, main='Subclonal cooccurrency distribution', xlab='number of samples', xlim=c(0, x.max), breaks=breaks.ss, prob=T, plot=F)
-h.nn <- hist(nn, main='Nonclonal cooccurrency distribution', xlab='number of samples', xlim=c(0, x.max), breaks=breaks.nn, prob=T, plot=F)
+x.max <- max(c(max(cs/as.numeric(args[2])), max(cc/as.numeric(args[2])), max(ss/as.numeric(args[2])), max(nn/as.numeric(args[2]))))
+y.max <- max(c(max(density(cs/as.numeric(args[2]))$y), max(density(cc/as.numeric(args[2]))$y), max(density(ss/as.numeric(args[2]))$y), max(density(nn/as.numeric(args[2]))$y)))
+breaks.cs <- seq(0, 1, by=0.01)
+breaks.cc <- seq(0, 1, by=0.01)
+breaks.ss <- seq(0, 1, by=0.01)
+breaks.nn <- seq(0, 1, by=0.01)
+h.cs <- hist(cs/as.numeric(args[2]), breaks=breaks.cs, plot=F)
+h.cc <- hist(cc/as.numeric(args[2]), breaks=breaks.cc, plot=F)
+h.ss <- hist(ss/as.numeric(args[2]), breaks=breaks.ss, plot=F)
+h.nn <- hist(nn/as.numeric(args[2]), breaks=breaks.nn, plot=F)
 
-svg(paste0(args[3], '/edge_hist.', args[2], '.svg'))
-hist(cs, main='Edge distribution', xlab='number of samples', ylim=c(0, y.max), xlim=c(0, x.max), breaks=breaks.cs, prob=T)
-lines(density(cs), col=4, lty=2)
+svg(paste0(args[4], '/edge_hist.', args[3], '.svg'))
+hist(cs/as.numeric(args[2]), main='Edge distribution', xlab='%samples', ylim=c(0, y.max), xlim=c(0, 1), breaks=breaks.cs, prob=T)
+lines(density(cs/as.numeric(args[2])), col=4, lty=2)
 dev.off()
 
-svg(paste0(args[3], '/clonal_cocc_hist.', args[2], '.svg'))
-hist(cc, main='Clonal cooccurrency distribution', xlab='number of samples', ylim=c(0, y.max), xlim=c(0, x.max), breaks=breaks.cc, prob=T)
-lines(density(cc), col=4, lty=2)
+svg(paste0(args[4], '/clonal_cocc_hist.', args[3], '.svg'))
+hist(cc/as.numeric(args[2]), main='Clonal cooccurrency distribution', xlab='%samples', ylim=c(0, y.max), xlim=c(0, 1), breaks=breaks.cc, prob=T)
+lines(density(cc/as.numeric(args[2])), col=4, lty=2)
 dev.off()
 
-svg(paste0(args[3], '/subclonal_cocc_hist.', args[2], '.svg'))
-hist(ss, main='Subclonal cooccurrency distribution', xlab='number of samples', ylim=c(0, y.max), xlim=c(0, x.max), breaks=breaks.ss, prob=T)
-lines(density(ss), col=4, lty=2)
+svg(paste0(args[4], '/subclonal_cocc_hist.', args[3], '.svg'))
+hist(ss/as.numeric(args[2]), main='Subclonal cooccurrency distribution', xlab='%samples', ylim=c(0, y.max), xlim=c(0, 1), breaks=breaks.ss, prob=T)
+lines(density(ss/as.numeric(args[2])), col=4, lty=2)
 dev.off()
 
-svg(paste0(args[3], '/nonclonal_cocc_hist.', args[2], '.svg'))
-hist(nn, main='Nonclonal cooccurrency distribution', xlab='number of samples', ylim=c(0, y.max), xlim=c(0, x.max), breaks=breaks.nn, prob=T)
-lines(density(nn), col=4, lty=2)
+svg(paste0(args[4], '/nonclonal_cocc_hist.', args[3], '.svg'))
+hist(nn/as.numeric(args[2]), main='Nonclonal cooccurrency distribution', xlab='%samples', ylim=c(0, y.max), xlim=c(0, 1), breaks=breaks.nn, prob=T)
+lines(density(nn/as.numeric(args[2])), col=4, lty=2)
 dev.off()
 
 t <- cbind(cs, cs+cc+ss+nn)
@@ -55,16 +55,36 @@ if(length(which(cs == 0)) != 0) t <- t[-which(cs == 0),]
 if(length(which((cs+cc+ss+nn) == 0)) != 0) t <- t[-which((cs+cc+ss+nn) == 0),]
 n.stop <- length(t[,1])
 ratio <- t[,1]/t[,2]
-lratio <- log(ratio)
+lratio <- log2(ratio)
 
 x.lim <- c(min(lratio), max(lratio))
 x.den <- density(lratio)$y
 breaks <- seq(round(min(lratio))-0.5, round(max(lratio))+0.5, by=0.2)
-h.ratio <- hist(lratio, xlim=x.lim, breaks=breaks, plot=F)
+h.ratio <- hist(lratio, breaks=breaks, plot=F)
 y.lim <- c(min(x.den), max(x.den, max(h.ratio$density)))
 
-svg(paste0(args[3], '/lratio_hist.', args[2], '.svg'))
-hist(lratio, xlim=x.lim, ylim=y.lim, breaks=breaks, main='Cooccurrency distribution', xlab='log(dependency/co_occurrency)', prob=T)
+svg(paste0(args[4], '/lratio_hist.', args[3], '.svg'))
+hist(lratio, xlim=c(-max(abs(lratio)), 0.5), ylim=y.lim, breaks=breaks, main='Cooccurrency distribution', xlab='log2(dependency/co_occurrency)', prob=T)
 lines(density(lratio), col=4, lty=2)
 dev.off()
 
+svg(paste0(args[4], '/lratio_scatter.', args[3], '.svg'))
+plot(lratio, (t[,1] + t[,2])/as.numeric(args[2]), xlim=c(-max(abs(lratio)), 0.5), ylim=c(0,1), main='Cooccurrency distribution in samples', xlab='log2(dependency/co_occurrency)', ylab='%samples', pch=20, col=rgb(0,0,0,.15))
+dev.off()
+
+t <- cbind(cs, cs+cc+ss+nn)
+diff <- t[,1] - t[,2]
+x.lim <- c(-max(abs(diff)), max(abs(diff)))
+x.den <- density(diff)$y
+breaks <- seq(round(min(diff))-0.5, round(max(diff))+0.5, by=0.2)
+h.diff <- hist(diff, breaks=breaks, plot=F)
+y.lim <- c(min(x.den), max(x.den, max(h.ratio$density)))
+
+svg(paste0(args[4], '/diff_hist.', args[3], '.svg'))
+hist(diff, xlim=c(-max(abs(lratio)), 0.5), ylim=y.lim, breaks=breaks, main='Cooccurrency distribution', xlab='dependency - co_occurrency', prob=T)
+lines(density(diff), col=4, lty=2)
+dev.off()
+
+svg(paste0(args[4], '/diff_scatter.', args[3], '.svg'))
+plot(diff, (t[,1] + t[,2])/as.numeric(args[2]), xlim=c(-max(abs(lratio)), 0.5), ylim=c(0,1), main='Cooccurrency distribution in samples', xlab='dependency - co_occurrency', ylab='%samples', pch=20, col=rgb(0,0,0,.15))
+dev.off()
--- scripts/plotOccurrency.R
+++ scripts/plotOccurrency.R
@@ -1,14 +1,14 @@
 #!/usr/bin/env Rscript
 #
-# ./plotOccurrency.R total_graph suffix output_dir
+# ./plotOccurrency.R total_graph n.samples suffix output_dir
 
 library('igraph')
 
 args <- commandArgs(trailingOnly=TRUE)
-if(length(args) != 3) stop('./plotOccurrency.R total_graph suffix output_dir')
+if(length(args) != 4) stop('./plotOccurrency.R total_graph n.samples suffix output_dir')
 
 # Mkdir
-if(!file.exists(args[3])) dir.create(args[3])
+if(!file.exists(args[4])) dir.create(args[4])
 
 # Read
 g <- read.graph(args[1], format='graphml')
@@ -16,21 +16,21 @@ g <- read.graph(args[1], format='graphml')
 co <- as.numeric(V(g)$clonal.occ)
 so <- as.numeric(V(g)$subclonal.occ)
 
-x.max <- max(max(co), max(so))
-y.max <- max(max(density(co)$y), max(density(so)$y))
-breaks.clo <- seq(-0.5, max(co)+1)
-breaks.sub <- seq(-0.5, max(co)+1)
-h.clo <- hist(co, main='Clonal status distribution', xlab='number of samples', xlim=c(0, x.max), breaks=breaks.clo, prob=T, plot=F)
-h.sub <- hist(so, main='Subclonal status distribution', xlab='number of samples', xlim=c(0, x.max), breaks=breaks.sub, prob=T, plot=F)
+x.max <- max(max(co/as.numeric(args[2])), max(so/as.numeric(args[2])))
+breaks.clo <- seq(0, 1, by=0.01)
+breaks.sub <- seq(0, 1, by=0.01)
+h.clo <- hist(co/as.numeric(args[2]), breaks=breaks.clo, plot=F)
+h.sub <- hist(so/as.numeric(args[2]), breaks=breaks.sub, plot=F)
+y.max <- max(max(density(co/as.numeric(args[2]))$y), max(density(so/as.numeric(args[2]))$y), max(h.clo$density), max(h.sub$density))
 
-svg(paste0(args[3], '/clonal_occ_hist.', args[2], '.svg'))
-hist(co, main='Clonal status distribution', xlab='number of samples', xlim=c(0, x.max), ylim=c(0, y.max), breaks=breaks.clo, prob=T)
-lines(density(co), col=4, lty=2)
+svg(paste0(args[4], '/clonal_occ_hist.', args[3], '.svg'))
+hist(co/as.numeric(args[2]), main='Clonal status distribution', xlab='%samples', xlim=c(0, 1), ylim=c(0, y.max), breaks=breaks.clo, prob=T)
+lines(density(co/as.numeric(args[2])), col=4, lty=2)
 dev.off()
 
-svg(paste0(args[3], '/subclonal_occ_hist.', args[2], '.svg'))
-hist(so, main='Subclonal status distribution', xlab='number of samples', xlim=c(0, x.max), ylim=c(0, y.max), breaks=breaks.sub, prob=T)
-lines(density(so), col=4, lty=2)
+svg(paste0(args[4], '/subclonal_occ_hist.', args[3], '.svg'))
+hist(so/as.numeric(args[2]), main='Subclonal status distribution', xlab='%samples', xlim=c(0, 1), ylim=c(0, y.max), breaks=breaks.sub, prob=T)
+lines(density(so/as.numeric(args[2])), col=4, lty=2)
 dev.off()
 
 t <- cbind(co, so)
@@ -41,16 +41,36 @@ n.stop <- length(t[,1])
 print(paste0('Removed ', n.start-n.stop, ' aberrations from ratio plot.'))
 
 ratio <- t[,1]/t[,2]
-lratio <- log(ratio)
+lratio <- log2(ratio)
 
 x.lim <- c(min(lratio), max(lratio))
 x.den <- density(lratio)$y
 breaks <- seq(round(min(lratio))-0.5, round(max(lratio))+0.5, by=0.2)
-h.ratio <- hist(lratio, xlim=x.lim, breaks=breaks, plot=F)
+h.ratio <- hist(lratio, breaks=breaks, plot=F)
 y.lim <- c(min(x.den), max(x.den, max(h.ratio$density)))
 
-svg(paste0(args[3], '/lratio_hist.', args[2], '.svg'))
-hist(lratio, xlim=x.lim, ylim=y.lim, breaks=breaks, main='Clonality status distribution', xlab='log(clonal_samples/subclonal_samples)', prob=T)
+svg(paste0(args[4], '/lratio_hist.', args[3], '.svg'))
+hist(lratio, xlim=c(-max(abs(lratio)), max(abs(lratio))), ylim=y.lim, breaks=breaks, main='Clonality status distribution', xlab='log2(clonal_samples/subclonal_samples)', prob=T)
 lines(density(lratio), col=4, lty=2)
 dev.off()
 
+svg(paste0(args[4], '/lratio_scatter.', args[3], '.svg'))
+plot(lratio, (t[,1] + t[,2])/as.numeric(args[2]), xlim=c(-max(abs(lratio)), max(abs(lratio))), main='Clonality status distribution in samples', ylim=c(0,1), ylab='%samples', xlab='log2(clonal_samples/subclonal_samples)', pch=20, col=rgb(0,0,0,.15))
+dev.off()
+
+t <- cbind(co, so)
+diff <- t[,1] - t[,2]
+x.lim <- c(-max(abs(diff)), max(abs(diff)))
+x.den <- density(diff)$y
+breaks <- seq(round(min(diff))-0.5, round(max(diff))+0.5, by=0.2)
+h.diff <- hist(diff, breaks=breaks, plot=F)
+y.lim <- c(min(x.den), max(x.den, max(h.diff$density)))
+
+svg(paste0(args[4], '/diff_hist.', args[3], '.svg'))
+hist(diff, xlim=c(-max(abs(lratio)), max(abs(lratio))), ylim=y.lim, breaks=breaks, main='Clonality status distribution', xlab='clonal_samples - subclonal_samples', prob=T)
+lines(density(diff), col=4, lty=2)
+dev.off()
+
+svg(paste0(args[4], '/diff_scatter.', args[3], '.svg'))
+plot(diff, (t[,1] + t[,2])/as.numeric(args[2]), xlim=c(-max(abs(lratio)), max(abs(lratio))), ylim=c(0,1), main='Clonality status distribution in samples', xlab='clonal_samples - subclonal_samples', ylab='%samples', pch=20, col=rgb(0,0,0,.15))
+dev.off()
